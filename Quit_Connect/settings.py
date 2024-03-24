3467803a6a7498e1c5598ae4a36c0e5870fd6735
AlexO <alexandreosorio18@gmail.com>
2024-03-24 20:42:20 +1100

    database on render attempt 2

--- Quit_Connect/settings.py
+++ Quit_Connect/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     },
     'secondary': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/Quit_Connect',
+        default='postgresql://postgres:postgres@localhost:8000/Quit_Connect',
         conn_max_age=600
     )
 }

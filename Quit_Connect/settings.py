09e0bd2a224d812b25adf41df98f4025b3474c9b
AlexO <alexandreosorio18@gmail.com>
2024-03-27 19:03:14 +1100

    render database attempt4

--- Quit_Connect/settings.py
+++ Quit_Connect/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=fxip)3s@43!p^jgps2(9mfqar==yo5%_qhm0^h*ehnli-xyx4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['quitconnect.onrender.com', 'localhost']
 
@@ -126,13 +126,12 @@ USE_TZ = True
 STATIC_URL = '/static/' 
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
+# if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+# and renames the files with unique names for each version to support long-term caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'

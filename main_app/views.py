66060349011de1fe4434d68cbf2a1ec72fdf5f48
AlexO <alexandreosorio18@gmail.com>
2024-03-30 11:20:25 +1100

    update
    Co-authored-by: babybabywolf <babybabywolf@users.noreply.github.com>

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-
 from django.contrib.auth import login
-
+import uuid # needed for generating random IDs
+import boto3 # AWS Python SDK
+import os # needed for accessing env vars
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from .models import Group, UserPost, Meeting
+from .models import Group, UserPost, Photo, Meeting
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.forms import ModelForm, CharField, FileField
 
 def home(request):
     return render(request, 'home.html')
@@ -58,17 +60,28 @@ def signup(request):
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
 
+class PostCreateForm(ModelForm):
+    class Meta:
+        fields=['content']
+        model = UserPost
+        
+    post_photo = FileField()
+
 
 class PostCreate(CreateView):
     model = UserPost
-    fields=['content']
+    # fields=['content']
+    form_class = PostCreateForm
+    
+    def form_valid(self, form):
+        print(form)
+        print(form.instance)
+        # print(form.instance.photo_set)
+        # print(form.instance.photo_file)
+        print(self.form_class.post_photo)
 
-    def form_valid(self, form): 
-        print(self)
-        print(self.model)
-        print(self.model.group)
         form.instance.user = self.request.user
-        form.instance.group_id = self.kwargs['group_id']
+        form.instance.group_id = self.kwargs['group_id'] 
 
         return super().form_valid(form)
 class PostDelete(DeleteView):
@@ -79,9 +92,26 @@ class PostDelete(DeleteView):
         return reverse_lazy('group_detail', kwargs={'group_id': group_id})
 
 class PostUpdate(UpdateView):
-  model = UserPost
-  fields = ['content'] 
+    model = UserPost
+    fields = ['content'] 
 
-def get_success_url(self):
-    group_id = self.object.group.id
-    return reverse_lazy('group_detail', kwargs={'group_id': group_id})
+    def get_success_url(self):
+        group_id = self.object.group.id
+        return reverse_lazy('group_detail', kwargs={'group_id': group_id})
+
+def add_photo(request, group_name, post_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            bucket = os.environ['S3_BUCKET']
+            s3.upload_fileobj(photo_file, bucket, key)
+            # build the full url string
+            url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            Photo.objects.create(url=url, post_id=post_id)
+        except Exception as e:
+            print('An error occurred uploading file to S3')
+            print(e)
+    return redirect('group_detail', post_id=post_id)
